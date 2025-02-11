628b7205192577ee400f3a651942a74d47dd0272
Kuruv Patel <kuruvpatel2005@gmail.com>
2025-02-11 21:58:22 +0530

    db

--- frontend/types/auth.ts
+++ frontend/types/auth.ts
@@ -2,11 +2,16 @@ interface UserData {
 	id: string;
 	email: string;
 	name: string;
+	phone: number;
+	image: string;
+	role: UserType;
 }
 
 interface JWTPayload {
 	userId: string;
 	email: string;
 }
+
+type UserType = "User" | "Admin";
 
 export type { UserData, JWTPayload };

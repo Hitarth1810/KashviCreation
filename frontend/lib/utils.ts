df9b854c71d6e3175720723072dfc53d9e454846
Hitarth1810 <hmshahbrd931@gmail.com>
2025-02-19 17:07:56 +0530

    changes to my-order

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 // Function to generate a custom numeric order ID
 export function generateOrderId(userObjectId: string): string {
 	const hexPart = userObjectId.substring(0, 4); // First 8 chars (timestamp)
-	const userNumericId = parseInt(hexPart, 4).toString(); // Extracted user ID
+	const userNumericId = parseInt(hexPart, 2).toString(); // Extracted user ID
 	const timestamp = Math.floor(Date.now() / 1000); // Unix timestamp in seconds
 	const randomPart = Math.floor(Math.random() * 100); // Random 2-digit number
 
